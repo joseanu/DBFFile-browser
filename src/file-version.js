be43efcc09ec647af91a6d4dc694070198653b02
Antonio Ulloa <joseanu@gmail.com>
2026-04-13 17:23:31 -0600

    refactor: remove deprecated DBF file handling code and update TypeScript configuration

--- src/file-version.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isValidFileVersion = isValidFileVersion;
-function isValidFileVersion(fileVersion) {
-    return [0x03, 0x83, 0x8b, 0x30, 0xf5].includes(fileVersion);
-}
-//# sourceMappingURL=file-version.js.map
