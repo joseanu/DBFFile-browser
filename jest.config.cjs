7f44e3a19ddb97eea6b2f7d3dae9e06aa8e37842
Antonio Ulloa <joseanu@gmail.com>
2026-04-13 16:10:54 -0600

    feat: add Jest configuration and self-reference script for module symlinking

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'test/tsconfig.json' }],
+  },
 };
